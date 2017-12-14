0b7de4d80209f55925ac3bd697cd99f8f013079d
Guest User <Guest@Epicodus-4B.local>
2017-12-14 09:35:58 -0800

    Split front end and backend.

--- build/js/app.js
+++ build/js/app.js
@@ -2,7 +2,7 @@
 exports.apiKey = "37d0e8276c217954be4c18124cc58e50";
 
 },{}],2:[function(require,module,exports){
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -12,92 +12,71 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+var apiKey = require('./../.env').apiKey;
+
 var Doctor = exports.Doctor = function () {
   function Doctor() {
     _classCallCheck(this, Doctor);
   }
 
   _createClass(Doctor, [{
-    key: "medicalIssue",
-    value: function medicalIssue(query, key) {}
-  }, {
-    key: "doctorSearch",
-    value: function doctorSearch(query, key) {}
+    key: 'getData',
+    value: function getData(type, query, displayData) {
+      $.get('https://api.betterdoctor.com/2016-03-01/doctors?' + type + '=' + query + '&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C122.6765&skip=0&limit=10&user_key=' + apiKey).then(function (response) {
+        displayData(response);
+      }).fail(function (error) {
+        error(error);
+      });
+    }
   }]);
 
   return Doctor;
 }();
 
-},{}],3:[function(require,module,exports){
+},{"./../.env":1}],3:[function(require,module,exports){
 "use strict";
 
 var _Doctor = require("./../js/Doctor.js");
 
-var apiKey = require('./../.env').apiKey;
+var displayData = function displayData(response) {
+  $("#output").empty();
+  if (response.data.length > 0) {
+    for (var i = 0; i < response.data.length; i++) {
+      $("#output").append("<li><span id=\"doc-name\">Name: " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</span><br>");
+      for (var j = 0; j < response.data[i].practices.length; j++) {
+        $("#output").append("<em><b>Practice " + (j + 1) + "<b><em><br>");
+        $("#output").append("Address: " + response.data[i].practices[j].visit_address.street + ", " + response.data[i].practices[j].visit_address.city + ", " + response.data[i].practices[j].visit_address.state + "<br>");
+        $("#output").append("Phone: " + response.data[i].practices[j].phones[0].number + "<br>");
+        if (response.data[i].practices[j].website != undefined) {
+          $("#output").append("Website: <a href='" + response.data[i].practices[j].website + "'>" + response.data[i].practices[j].website + "</a><br>");
+        } else {
+          $("#output").append("Website: None Listed<br>");
+        }
+        if (response.data[i].practices[j].accepts_new_patients) {
+          $("#output").append("Currently Accepting New Patients<br>");
+        } else {
+          $("#output").append("Not Accepting New Patients Currently<br>");
+        }
+      }
+      $("#output").append("</li>");
+    }
+  } else {
+    $("#output").append("Sorry, nothing matches that search.");
+  }
+};
+var error = function error() {
+  $("#output").append("" + error.meta.message);
+};
 $(document).ready(function () {
+  var finder = new _Doctor.Doctor();
   $("#health-search").click(function () {
     var query = $("#health-problem").val();
-    $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + query + "&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C122.6765&skip=0&limit=10&user_key=" + apiKey).then(function (response) {
-      console.log(response);
-      $("#output").empty();
-      if (response.data.length > 0) {
-        for (var i = 0; i < response.data.length; i++) {
-          $("#output").append("<li><span id=\"doc-name\">Name: " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</span><br>");
-          for (var j = 0; j < response.data[i].practices.length; j++) {
-            $("#output").append("<em><b>Practice " + (j + 1) + "<b><em><br>");
-            $("#output").append("Address: " + response.data[i].practices[j].visit_address.street + ", " + response.data[i].practices[j].visit_address.city + ", " + response.data[i].practices[j].visit_address.state + "<br>");
-            $("#output").append("Phone: " + response.data[i].practices[j].phones[0].number + "<br>");
-            if (response.data[i].practices[j].website != undefined) {
-              $("#output").append("Website: <a href='" + response.data[i].practices[j].website + "'>" + response.data[i].practices[j].website + "</a><br>");
-            } else {
-              $("#output").append("Website: None Listed<br>");
-            }
-            if (response.data[i].practices[j].accepts_new_patients) {
-              $("#output").append("Currently Accepting New Patients<br>");
-            } else {
-              $("#output").append("Not Accepting New Patients Currently<br>");
-            }
-          }
-          $("#output").append("</li>");
-        }
-      } else {
-        $("#output").append("Sorry, nothing matches that search.");
-      }
-    }).fail(function (error) {
-      $("#output").append("" + error.meta.message);
-    });
+    finder.getData("query", query, displayData);
   });
   $("#doctor-search").click(function () {
     var query = $("#doctor-name").val();
-    $.get("https://api.betterdoctor.com/2016-03-01/doctors?name=" + query + "&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C122.6765&skip=0&limit=10&user_key=" + apiKey).then(function (response) {
-      $("#output").empty();
-      if (response.data.length > 0) {
-        for (var i = 0; i < response.data.length; i++) {
-          $("#output").append("<li><span id=\"doc-name\">Name: " + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</span><br>");
-          for (var j = 0; j < response.data[i].practices.length; j++) {
-            $("#output").append("<em><b>Practice " + (j + 1) + "<b><em><br>");
-            $("#output").append("Address: " + response.data[i].practices[j].visit_address.street + ", " + response.data[i].practices[j].visit_address.city + ", " + response.data[i].practices[j].visit_address.state + "<br>");
-            $("#output").append("Phone: " + response.data[i].practices[j].phones[0].number + "<br>");
-            if (response.data[i].practices[j].website != undefined) {
-              $("#output").append("Website: <a href='" + response.data[i].practices[j].website + "'>" + response.data[i].practices[j].website + "</a><br>");
-            } else {
-              $("#output").append("Website: None Listed<br>");
-            }
-            if (response.data[i].practices[j].accepts_new_patients) {
-              $("#output").append("Currently Accepting New Patients<br>");
-            } else {
-              $("#output").append("Not Accepting New Patients Currently<br>");
-            }
-          }
-          $("#output").append("</li>");
-        }
-      } else {
-        $("#output").append("Sorry, nothing matches that search.");
-      }
-    }).fail(function (error) {
-      $("#output").append("" + error.meta.message);
-    });
+    finder.getData("name", query, displayData);
   });
 });
 
-},{"./../.env":1,"./../js/Doctor.js":2}]},{},[3]);
+},{"./../js/Doctor.js":2}]},{},[3]);
--- js/Doctor.js
+++ js/Doctor.js
@@ -1,5 +1,14 @@
+var apiKey = require('./../.env').apiKey;
 export class Doctor {
   constructor() {
 
+  }
+
+  getData(type, query, displayData) {
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?${type}=${query}&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C122.6765&skip=0&limit=10&user_key=${apiKey}`).then(function(response) {
+      displayData(response);
+    }).fail(function(error) {
+      error(error);
+    });
   }
 }
